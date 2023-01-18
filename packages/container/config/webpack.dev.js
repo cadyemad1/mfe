1a47b0bee89c5e058aaaa5a91488671fcd9dcfc9
omar0404 <cady.emad@stud.fci-cu.edu.eg>
2023-01-18 18:32:21 +0200

    add container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,9 +1,8 @@
 const {merge} = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require ('./webpack.common');
-import {dependencies} from '../package.json';
+const packageJSON = require('../package.json')
 
 const devConfig = {
     mode:'development',
@@ -19,11 +18,8 @@ const devConfig = {
            remotes:{
             marketing:'marketing@http://localhost:8081/remoteEntry.js'
            },
-           shared:dependencies
+           shared:packageJSON.dependencies
 
-        }),
-        new HtmlWebpackPlugin({
-            template:'./public/index.html'
         })
     ]
 };
